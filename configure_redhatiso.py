53984b3499763c0b177f60fb6fc8b3067c1cce50
anant <anant@localhost.localdomain>
2020-04-16 15:01:47 -0400

    resolves

--- configure_redhatiso.py
+++ configure_redhatiso.py
@@ -1,16 +1,16 @@
 import os
-
+name = input(os.system("whoami"))
 os.system("touch /etc/yum.repos.d/base1.repo")
 file = open("/etc/yum.repos.d/base1.repo","w")
 file.write(""" 
 [dvd1]
-baseurl=file:///run/media/anant/RHEL-8-0-0-BaseOS-x86_64/AppStream
+baseurl=file:///run/media/{0}/RHEL-8-0-0-BaseOS-x86_64/AppStream
 gpgcheck=0
 
 [dvd2]
-baseurl=file:///run/media/anant/RHEL-8-0-0-BaseOS-x86_64/BaseOS
+baseurl=file:///run/media/{0}/RHEL-8-0-0-BaseOS-x86_64/BaseOS
 gpgcheck=0
 
-""")
+""").format(name)
 file.close()
 os.system("yum update")
